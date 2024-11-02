e00ab5f89a5d589e50e3e4b67fbd5e641dea6de4
Yannick-N <yannick.napoleon.pro@gmail.com>
2024-11-02 15:12:11 +0100

    NY-16 : refacto schema & routing policy

--- app/routes/policy_route.py
+++ app/routes/policy_route.py
@@ -1,40 +1,51 @@
 from flask import Blueprint, jsonify, request
-from app.services.policy_service import create_policy, get_policy, update_policy, delete_policy
+from marshmallow import ValidationError
+from app.schemas.policy_schema import PolicySchema
+from app.services.policy_service import create_policy, get_policies_of_firewall, update_policy, delete_policy
+
+policy_schema = PolicySchema()
 
 policy_bp = Blueprint('policy', __name__)
 
-@policy_bp.route('/', methods=['POST'])
-def handle_create_policy():
+@policy_bp.route('/<int:firewall_id>/policies', methods=['POST'])
+def handle_create_policy(firewall_id):
     try:
-        policy = create_policy(request.get_json())
+        validated_data = policy_schema.load(request.get_json())
+        validated_data["firewall_id"] = firewall_id 
+        policy = create_policy(validated_data)
         return jsonify(policy.to_dict()), 201
-    except ValueError as ve:
-        return jsonify(ve.args[0]), 400
+    except ValidationError as err:
+        return jsonify({"error": err.messages})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@policy_bp.route('/<int:policy_id>', methods=['GET'])
-def handle_get_policy(policy_id):
+@policy_bp.route('<int:firewall_id>/policies', methods=['GET'])
+def handle_get_policies(firewall_id):
     try:
-        policy = get_policy(policy_id)
-        if policy:
-            return jsonify(policy.to_dict()), 200
-        return jsonify({"error": "Policy not found"}), 404
+        policies = get_policies_of_firewall(firewall_id)
+        if policies:
+            return jsonify([policy.to_dict() for policy in policies]), 200
+        return jsonify({"error": "No policies found for this firewall"}), 404
     except ValueError as e:
         return jsonify({'error': str(e)}), 404
-    
-@policy_bp.route('/<int:policy_id>', methods=['POST'])
-def handle_update_policy(policy_id):
+
+
+@policy_bp.route('<int:firewall_id>/policies/<int:policy_id>', methods=['POST'])
+def handle_update_policy(firewall_id, policy_id):
     try:
-        updated_policy = update_policy(policy_id, request.get_json())
+
+        validated_data = policy_schema.load(request.get_json())
+        validated_data["firewall_id"] = firewall_id
+        validated_data["policy_id"] = policy_id
+        updated_policy = update_policy(validated_data)
         return jsonify(updated_policy.to_dict()), 200
     except ValueError as e:
-        return jsonify({"error": str(e)}), 404
+        return jsonify({"error": str(e)}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@policy_bp.route('/<int:policy_id>', methods=['DELETE'])
-def handle_delete_policy(policy_id):
+@policy_bp.route('<int:firewall_id>/policies/<int:policy_id>', methods=['DELETE'])
+def handle_delete_policy(firewall_id, policy_id):
     try:
         delete_policy(policy_id)
         return jsonify({"message": f"Policy with ID {policy_id} has been deleted."}), 204
--- app/services/policy_service.py
+++ app/services/policy_service.py
@@ -1,22 +1,13 @@
-from marshmallow import ValidationError
 from app import db
 from app.models.policy import Policy
 from app.models.firewall import Firewall
-from app.schemas.policy_schema import PolicySchema
-
-policy_schema = PolicySchema()
 
 def create_policy(data):
-    try:
-        validated_data = policy_schema.load(data)
-    except ValidationError as err:
-        raise ValueError({"error": err.messages})
-
     firewall = db.session.get(Firewall, data['firewall_id'])
     if not firewall:
         raise ValueError(f"Firewall with ID {data['firewall_id']} does not exist.")
     
-    status = validated_data.get('status', 'active')
+    status = data.get('status', 'active')
     if status not in ['active', 'inactive']:
         raise ValueError("Invalid status. Must be either 'active' or 'inactive'.")
     
@@ -25,30 +16,27 @@ def create_policy(data):
         raise ValueError(f"A policy with the name '{data['name']}' already exists for this firewall.")
     
     policy = Policy(
-        name=validated_data['name'],
-        firewall_id=validated_data['firewall_id'],
+        name=data['name'],
+        firewall_id=data['firewall_id'],
         status=status,
     )
     db.session.add(policy)
     db.session.commit()
     return policy
 
-def get_policy(policy_id):
-    return db.session.get(Policy, policy_id)
+def get_policies_of_firewall(firewall_id):
+    return Policy.query.filter_by(firewall_id=firewall_id).all()
 
-def update_policy(policy_id, data):
-    policy = db.session.get(Policy, policy_id)
-    if not policy:
-        raise ValueError(f"Policy with ID {policy_id} does not exist.")
-
+def update_policy(data):
+    policy = db.session.get(Policy, data["policy_id"])
     new_name = data.get('name', policy.name)
     existing_policy = db.session.query(Policy).filter_by(name=new_name).first()
+    
     if existing_policy and existing_policy.id != policy.id:
         raise ValueError(f"A policy with the name '{new_name}' already exists.")
 
     policy.name = new_name
-    policy.firewall_id = data.get('firewall_id', policy.firewall_id)
-    policy.status = data.get('status', policy.status)
+    policy.status = data.get('status', policy.status)    
     db.session.commit()
     return policy
 
